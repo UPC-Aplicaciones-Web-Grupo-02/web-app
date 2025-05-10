23e14d48418d001c8d8d4c82a053444f13a83763
Nestor Velarde <u20211c221@upc.edu.pe>
2025-05-09 21:20:31 -0500

    feat: develop login and create fake api

---
+++ src/movix/profiles/model/users.entity.js
@@ -0,0 +1,12 @@
+export class user {
+  constructor (name, dni, age, phone, email, password, role, photo) {
+    this.names = name;
+    this.dni = dni;
+    this.age = age;
+    this.phone = phone;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+    this.photo = photo;
+  }
+}
---
+++ src/movix/profiles/services/users.services.js
@@ -0,0 +1,18 @@
+import {BaseService} from "@/shared/base.service.js"
+
+export class Db {
+  signIn(email, password) {
+    return BaseService.http.get(`/user/signin?email=${email}&password=${password}`);
+  }
+  signUp(user){
+    return BaseService.http.post("/user",user)
+  }
+
+  editUser(id,user){
+    return BaseService.http.put("/user/" + id,user)
+  }
+
+  deleteUser(id){
+    return BaseService.http.delete("/user/" + id)
+  }
+}
