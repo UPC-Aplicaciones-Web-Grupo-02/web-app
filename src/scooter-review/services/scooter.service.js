95a5101d8ee0885267523dce7f80d01cbc0185db
RafaelCuya <u201913495@upc.edu.pe>
2025-05-14 17:03:49 -0500

    Feat(scooter-review) added services

---
+++ src/scooter-review/services/scooter.service.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/scooters'; // Ajusta según tu backend
+
+export default {
+    async getAll() {
+        const response = await axios.get(API_URL);
+        return response.data;
+    },
+
+    async getByRegion(region) {
+        const response = await axios.get(`${API_URL}?region=${region}`);
+        return response.data;
+    },
+
+    async getById(id) {
+        const response = await axios.get(`${API_URL}/${id}`);
+        return response.data;
+    },
+
+    async update(id, data) {
+        const response = await axios.put(`${API_URL}/${id}`, data);
+        return response.data;
+    },
+
+    async getReviews(id) {
+        const response = await axios.get(`${API_URL}/${id}/reviews`);
+        return response.data;
+    }
+};
