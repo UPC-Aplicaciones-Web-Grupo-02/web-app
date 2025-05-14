85bae4fdc90bd9566f1be4f4c504f4c2cea09111
RafaelCuya <u201913495@upc.edu.pe>
2025-05-14 16:52:43 -0500

    Feat(scooter-review) added main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import App from './App.vue'
 import PrimeVue from './primevue.js'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css';
+import router from './routes/routes.js'
 
-const app = createApp(App);
+import Button from 'primevue/button'
+import Card from 'primevue/card'
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Select from 'primevue/select';
 
-PrimeVue(app);
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.use(router)
+
+app.component('pv-button', Button)
+app.component('pv-card', Card)
+app.component('pv-inputText', InputText)
+app.component('pv-inputNumber', InputNumber)
+app.component('pv-select', Select)
 
 app.mount('#app');
