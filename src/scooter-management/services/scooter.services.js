5176a8014df1af22b652da4c113757cf909992fc
Sebastian <u201611430@upc.edu.pe>
2025-05-15 12:07:59 -0500

    feat: add updated scooter.services

--- src/scooter-management/services/scooter.services.js
+++ src/scooter-management/services/scooter.services.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 const http = axios.create({
   baseURL: 'http://localhost:3000',
 });
-export class ScooterApiService {
 
+export class ScooterApiService {
   getScooters() {
     return http.get('/scooters');
+  }
+
+  updateScooter(id, data) {
+    // Asegúrate de que id es numérico
+    return http.put(`/scooters/${Number(id)}`, data);
   }
 }
