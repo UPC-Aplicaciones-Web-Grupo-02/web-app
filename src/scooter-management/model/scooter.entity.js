53b708af1265b580facf045cf4984027f1034af0
Sebastian <u201611430@upc.edu.pe>
2025-05-15 11:29:18 -0500

    feat: add scooter entity.

---
+++ src/scooter-management/model/scooter.entity.js
@@ -0,0 +1,13 @@
+export class Scooter {
+  constructor(id, name, brand, model, price, direction, contact, image, detail){
+    this.id = id;
+    this.name = name;
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.direction = direction;
+    this.contact = contact;
+    this.image = image;
+    this.detail = detail;
+  }
+}
