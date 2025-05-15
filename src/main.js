49c9e2472447934b43138bb0eb389b3bc19658a4
Sebastian <u201611430@upc.edu.pe>
2025-05-15 12:08:53 -0500

    feat: add components primevue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import PrimeVue from './primevue.js'
+import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css';
 
+import {Button, Card, Toolbar, InputText, InputNumber} from "primevue";
+
 const app = createApp(App);
+app
+  .use(PrimeVue, {ripple: true, theme: {preset:Aura}})
+  .component('pv-button', Button)
+  .component('pv-card', Card)
+  .component('pv-toolbar', Toolbar)
+  .component('pv-inputtext', InputText)
+  .component('pv-inputnumber', InputNumber)
+  .mount('#app')
 
-PrimeVue(app);
-
-app.mount('#app');
