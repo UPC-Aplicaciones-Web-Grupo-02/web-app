46e4d89681c31b1d151ba908d83668729e123ca8
Nestor Velarde <u20211c221@upc.edu.pe>
2025-05-29 21:30:47 -0500

    feat: modifiqué las rutas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,8 @@ import LoginForm from "@/movix/profile-management/component/loginUser.component.
 import UserEditor from "@/movix/profile-management/component/editorUser.component.vue";
 import Profile from "@/movix/profile-management/component/viewUser.component.vue";
 import NotFound from "@/shared/components/notFound.component.vue";
-import ListScooterComponent from "@/movix/scooter-view/component/listScooter.component.vue";
-import DetailsScooter from "@/movix/scooter-view/component/scooterDatails.component.vue";
-import createScooter from "@/movix/scooter-view/component/createScooter.component.vue"
-import SubscriptionsPage from '@/movix/subscriptions/views/SubscriptionsPage.vue'
-import SubscriptionCurrent from '@/movix/subscriptions/components/SubscriptionCurrent.vue'
+import SubscriptionsPage from '@/movix/subscriptions/views/SubscriptionsPage.vue';
+import SubscriptionCurrent from '@/movix/subscriptions/components/SubscriptionCurrent.vue';
 import AddScooter from "@/movix/scooter-management/components/scooter-add.component.vue";
 import viewScooter from "@/movix/scooter-management/components/scooter-item.component.vue";
 import listScooter from "@/movix/scooter-management/components/scooter-list.component.vue"
@@ -26,16 +23,12 @@ const routes = [
 
     children: [
       { path: '/profile', component: Profile  },
-      {path: '/editor', component: UserEditor},
-
+      { path: '/editor', component: UserEditor},
       { path: '/suscriptions', component: SubscriptionsPage },
       { path: '/mi-suscripcion', component: SubscriptionCurrent },
-      {path: '/scooter', component: ListScooterComponent},
-      {path: '/detalle', component: DetailsScooter},
-      {path: '/create', component: createScooter}
-      {path: '/scooter', component: AddScooter},
-      {path: '/detalle', component: viewScooter},
-      {path: '/create', component: listScooter}
+      { path: '/scooter', component: AddScooter},
+      { path: '/detalle', component: viewScooter},
+      { path: '/create', component: listScooter}
     ]
   },
   { path: '/:pathMatch(.*)*', name:'NotFound', component: NotFound }
