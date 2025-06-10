ce728faac2aad00cda7c33ff6b754281be70de70
RafaelCuya <u201913495@upc.edu.pe>
2025-06-10 14:16:20 -0500

    feat(reservation) Added new information of reservation.services

--- src/movix/reservation/services/reservation.services.js
+++ src/movix/reservation/services/reservation.services.js
@@ -1,1 +1,15 @@
-// Pegas aqui el código Rafael
+import { BaseServices } from '../../../shared/BaseService.js';
+import Reservation from '../model/reservation.js';
+
+export default class reservationService {
+
+    static async MostrarReservation() {
+        const response = await BaseServices.http.get('/reservations');
+        return response.data.map(reservation => new Reservation(reservation));
+    }
+
+    static async createReservation(reservation) {
+        const response = await BaseServices.http.post('/reservations', reservation);
+        return new Reservation(response.data);
+    }
+}
