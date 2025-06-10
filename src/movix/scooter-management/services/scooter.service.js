3c46e1635efb6b572b290a2fc5318f54bbf2ce5a
Sebas2999 <102195501+Sebas2999@users.noreply.github.com>
2025-06-10 17:05:44 -0500

    Update scooter.service.js

--- src/movix/scooter-management/services/scooter.service.js
+++ src/movix/scooter-management/services/scooter.service.js
@@ -1,1 +1,27 @@
-// Pegar código aqui Sebastian
+import { BaseServices } from '../../../shared/BaseService.js';
+import Scooter from '../model/scooter.js';
+
+export default class ScooterService {
+
+    // Esta es para obtener la lista de personas
+    static async MostrarScooter() {
+        const response = await BaseServices.http.get('/scooters');
+        return response.data.map(scooter => new Scooter(scooter));
+    }
+
+    // Esta es para obtener una scooter por su ID
+    static async MostrarScooterPorId(id) {
+        const response = await BaseServices.http.get(/scooters/${id});
+        return new Scooter(response.data);
+    }
+
+    static async CrearScooter(scooter) {
+        const response = await BaseServices.http.post('/scooters', scooter);
+        return new Scooter(response.data);
+    }
+
+    static async EliminarScooter(id) {
+        const response = await BaseServices.http.delete(/scooters/${id});
+        return response.data;
+    }
+}
