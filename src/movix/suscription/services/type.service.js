5cceee47638ba96dd429d76544e07f82f887902e
Arnold24x24 <argamoso24@hotmail.com>
2025-06-10 19:27:33 -0500

    Update type.service.js (BaseServices)

--- src/movix/suscription/services/type.service.js
+++ src/movix/suscription/services/type.service.js
@@ -1,1 +1,16 @@
-// Pegar el código aqui Arnold
+import { BaseServices } from '../../../shared/BaseService.js';
+import Type from '../model/typeSuscription.js';
+import Suscription from '../model/suscription.js';
+
+export default class typeService {
+
+    static async MostrarSuscription() {
+        const response = await BaseServices.http.get('/type_suscription');
+        return response.data.map(type => new Type(type));
+    }
+
+    static async RegistrarSuscription(suscription) {
+        const response = await BaseServices.http.post('/suscriptions', suscription);
+        return new Suscription(response.data);
+    }
+}
